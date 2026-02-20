b339c11b8d2d51d7fb3e8c4ce9f7b8cfdf968d21
OdinStack <raiyanshikoh@gmail.com>
2026-02-20 20:28:03 +0530

    OOPS banner using for-each loop

--- Main.java
+++ Main.java
@@ -1,17 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-		String[] banner = {
-			String.join(" "," ********  " , " ********  " , "********  " , " ******** "),
-			String.join(" ","*        * " , "*        * " , "*       * " , "*        *"),
-			String.join(" ","*        * " , "*        * " , "*       * " , "*         "),
-			String.join(" ","*        * " , "*        * " , "********  " , " ******** "),
-			String.join(" ","*        * " , "*        * " , "*         " , "         *"),
-			String.join(" ","*        * " , "*        * " , "*         " , "*        *"),
-			String.join(" "," ********  " , " ********  " , "*         " , " ******** ")
-		};
+		String[] lines = new String[7];
+		lines[0]=String.join(" "," ********  " , " ********  " , "********  " , " ******** ");
+		lines[1]=String.join(" ","*        * " , "*        * " , "*       * " , "*        *");
+		lines[2]=String.join(" ","*        * " , "*        * " , "*       * " , "*         ");
+		lines[3]=String.join(" ","*        * " , "*        * " , "********  " , " ******** ");
+		lines[4]=String.join(" ","*        * " , "*        * " , "*         " , "         *");
+		lines[5]=String.join(" ","*        * " , "*        * " , "*         " , "*        *");
+		lines[6]=String.join(" "," ********  " , " ********  " , "*         " , " ******** ");
 		
-		for (String line : banner){
+		for (String line : lines){
 			System.out.println(line);
 		}
 
