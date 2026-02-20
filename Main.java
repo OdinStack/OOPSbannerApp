9ea180104ccb2f6ea02abf5301d37a2f5b08b5de
OdinStack <raiyanshikoh@gmail.com>
2026-02-20 21:34:30 +0530

    Defined functions for each letter

--- Main.java
+++ Main.java
@@ -1,20 +1,55 @@
 public class Main {
+	
+	
+    public static String[] getOPattern() {
+        return new String[] {
+            " ******* ",
+            "*       *",
+            "*       *",
+            "*       *",
+            "*       *",
+            "*       *",
+            " ******* "
+        };
+    }
+
+    public static String[] getPPattern() {
+        return new String[] {
+            "******** ",
+            "*       *",
+            "*       *",
+            "******** ",
+            "*        ",
+            "*        ",
+            "*        "
+        };
+    }
+
+    public static String[] getSPattern() {
+        return new String[] {
+            " *******",
+            "*       *",
+            "*        ",
+            " ******* ",
+            "        *",
+            "*       *",
+            " ******* "
+        };
+    }
+
     public static void main(String[] args) {
 
-		String[] lines = {
-			String.join(" "," ********  " , " ********  " , "********  " , " ******** ");
-			String.join(" ","*        * " , "*        * " , "*       * " , "*        *");
-			String.join(" ","*        * " , "*        * " , "*       * " , "*         ");
-			String.join(" ","*        * " , "*        * " , "********  " , " ******** ");
-			String.join(" ","*        * " , "*        * " , "*         " , "         *");
-			String.join(" ","*        * " , "*        * " , "*         " , "*        *");
-			String.join(" "," ********  " , " ********  " , "*         " , " ******** ");
-		};
-		
-		
-		for (String i : lines){
-			System.out.println(i);
-		}
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
 
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(
+                oPattern[i] + " " +
+                oPattern[i] + " " +
+                pPattern[i] + " " +
+                sPattern[i]
+            );
+        }
     }
 }
